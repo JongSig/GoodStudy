0a6a5800d441954e0c2a908d2c19af6004af5a26
JongSig <bnghty46@naver.com>
2024-11-30 20:38:16 +0900

    [Commit] 예제9-04

---
+++ 예제 9-04/src/MouseListenerEx.java
@@ -0,0 +1,60 @@
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+public class MouseListenerEx extends JFrame{
+	private JLabel label = new JLabel("Hello");
+	
+	public MouseListenerEx() {
+		setTitle("Mouse 이밴트");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		Container contentPane = getContentPane();
+		
+		contentPane.setLayout(null);
+		label.setSize(50, 20);
+		label.setLocation(30, 30);
+		contentPane.add(label);
+		
+		setSize(200, 200);
+		setVisible(true);
+		
+		contentPane.addMouseListener(new MouseListener() {
+			
+			@Override
+			public void mousePressed(MouseEvent e) {
+				// TODO Auto-generated method stub
+				int x = e.getX();
+				int y = e.getY();
+				label.setLocation(x, y);
+			}
+			
+			@Override
+			public void mouseReleased(MouseEvent e) {
+				// TODO Auto-generated method stub
+				
+			}
+			
+			@Override
+			public void mouseExited(MouseEvent e) {
+				// TODO Auto-generated method stub
+				
+			}
+			
+			@Override
+			public void mouseEntered(MouseEvent e) {
+				// TODO Auto-generated method stub
+				
+			}
+			
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				// TODO Auto-generated method stub
+				
+			}
+		});
+	}
+	
+	public static void main(String[] args) {
+		new MouseListenerEx();
+	}
+}
